d523234376b3885007fdc4cc747f85d338ae9ee3
DenizBicer <dnzbicer@gmail.com>
2019-02-03 19:58:01 +0100

    added the leap provider and hand models to the scene

---
+++ Assets/Scripts/LeapMotion/Interaction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+
+[RequireComponent(typeof(LeapServiceProvider))]
+public class Interaction : MonoBehaviour
+{
+    private LeapServiceProvider provider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        provider = GetComponent<LeapServiceProvider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
